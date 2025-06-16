a40e65dd7a84b54af897aaa72966b13d974a28d5
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-06-17 05:03:57 +0530

    fix: gitignore updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -12,13 +12,13 @@ import { getAnalytics } from "firebase/analytics";
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG4q9k4vthoh6ab7WeP2bhbPprgRHgD6A",
-  authDomain: "fitlife-b6824.firebaseapp.com",
-  projectId: "fitlife-b6824",
-  storageBucket: "fitlife-b6824.firebasestorage.app",
-  messagingSenderId: "551140582610",
-  appId: "1:551140582610:web:182664db8e68a7976b790b",
-  measurementId: "G-BZL559LBT3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // const analytics = getAnalytics(app);
