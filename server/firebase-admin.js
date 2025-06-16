2c4c9a1acffc9d52d10dc92d11b9272109b371fe
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-06-17 04:34:19 +0530

    fix: remove unnecessary comment in firebase-admin.js

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY); // Download from Firebase Console
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
+
